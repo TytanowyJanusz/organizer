b4a68132ee413ece89b688e751306b647d6edd9f
Hubert Lepczyński <lepczynski.hubert@gmail.com>
2020-01-01 19:55:40 +0100

    Configuring Spring Rest

--- src/main/java/com/lepczynski/hubert/organizer/OrganizerApplication.java
+++ src/main/java/com/lepczynski/hubert/organizer/OrganizerApplication.java
@@ -4,31 +4,31 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.support.WebStack;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableHypermediaSupport(stacks = WebStack.WEBMVC, type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
-public class OrganizerApplication {
+public class OrganizerApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrganizerApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrganizerApplication.class, args);
+    }
 
-
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("PUT", "DELETE","GET","POST")
-                        //.allowedHeaders("header1", "header2", "header3")
-                       // .exposedHeaders("header1", "header2")
-                        .allowCredentials(true).maxAge(3600);
-            }
-        };
+    public class RestConfig implements RepositoryRestConfigurer
+    {
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration restConfig)
+        {
+            restConfig.setBasePath("organizer");
+        }
     }
 }
